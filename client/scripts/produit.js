24e1ef1363f049e1c4135d52f207a39b53bcbdef
[nadf0901] <[nadf0901@usherbrooke.ca]>
2022-11-15 17:50:58 -0500

    labo5 terminé

--- client/scripts/produit.js
+++ client/scripts/produit.js
@@ -14,14 +14,20 @@ function item_to_html(item){
         .append(item_detail)
         .append(' <p class="card-text"> ' + item.description + '</p>');
     item_footer = $('<div style="text-align: center"></div>')
-        .append('<i class="fa-solid fa-cart-plus fa-3x" style="padding-bottom: 1rem"></i>');
+        .append('<p class="w-100 display-6 text-center">\n' +
+            '<button type="button" class="btn btn-primary position-relative" onclick="add_item(' + item.id + ')">\n' +
+            '<i class="bi bi-cart-plus"></i>\n' +
+            '</button>\n' +
+            '</p>');
     item_card.append(item_head).append(item_body).append(item_footer);
     return $('<div></div>').addClass('col-md-3').append(item_card);
 }
 
 function chargerproduit(){
-    $('#list_items').append('<p> Ceci est un paragraphe avec jQuery</p>');
-    $('<div></div>').addClass('card mb-4 text-center');
+    let ID_CLIENT = 1;
+    let TOKEN_CLIENT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZENsaWVudCI6MSwicm9sZSI6ImNsaWVudCIsImlhdCI6MTYzNjc1MjI1MywiZXhwIjoxODM2NzUyMjUzfQ.qMcKC0NeuVseNSeGtyaxUvadutNAfzxlhL5LYPsRB8k";
+
+    $('<div></div>').addClass('container mb-4 text-center');
     $.ajax({
         url: "/produits",
         success: function( result ) {
@@ -30,6 +36,33 @@ function chargerproduit(){
                 item = item_to_html(value);
                 $('#list_items').append(item);
             });
+            $('#item_counter').append()
+        }
+    });
+
+    $.ajax({
+        url: "/clients/"+ID_CLIENT+"/panier",
+        beforeSend: function (xhr){
+            xhr.setRequestHeader('Authorization', "Basic "+ TOKEN_CLIENT);
+        },
+        success: function( result ) {
+            $('#item_counter').text(result.items.length);
+        }
+    });
+}
+
+function add_item(id_item){
+    let ID_CLIENT = 1;
+    let TOKEN_CLIENT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZENsaWVudCI6MSwicm9sZSI6ImNsaWVudCIsImlhdCI6MTYzNjc1MjI1MywiZXhwIjoxODM2NzUyMjUzfQ.qMcKC0NeuVseNSeGtyaxUvadutNAfzxlhL5LYPsRB8k";
+    $.ajax({
+        url: "/clients/"+ID_CLIENT+"/panier",
+        method:"POST",
+        data: {"idProduit": id_item, "quantite": 1},
+        beforeSend: function (xhr){
+            xhr.setRequestHeader('Authorization', "Basic "+ TOKEN_CLIENT);
+        },
+        success: function( result ) {
+            $('#item_counter').text(result.items.length);
         }
     });
 }
