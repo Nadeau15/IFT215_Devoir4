4e41be6b086329b6ef3d0148529e15fc3c7f2f6d
Simon Breton <bres2102@usherbrooke.ca>
2022-12-02 17:22:28 -0500

    Commit qte_inventaire

--- client/scripts/panier.js
+++ client/scripts/panier.js
@@ -7,7 +7,7 @@ function panier_to_html(item){
             .append('<p class="col">'  + '</p>')                        
             .append('<p class="col">' + '<input type="button" id="'+ item.id +'" onclick="enleverItem('+ item.id +')" min="0" width= "10px;" value="-"' + item.quantite + '"/>' + '</p>')
             .append('<p class="col" id="qte'+ item.id +'">' + item.quantite + '</p>')
-            .append('<p class="col">' + '<input type="button" id="'+ item.id +'" onclick="ajouterItem('+ item.id +')" min="0" width= "10px;" value="+"/>' + '</p>')
+            .append('<p class="col">' + '<input type="button" id="'+ item.id +'" onclick="ajouterItem('+ item.id+ ","+ item.idProduit +')" min="0" width= "10px;" value="+"/>' + '</p>')
             .append('<p class="col" id="total'+ item.id +'">' + (item.prix * item.quantite).toFixed(2) +'</p>')            
             .append('<p class="col" id="bouton">'+ '<button type="button" id="retirer'+item.id+' "class="infoButton" onclick="retirerPanier('+item.id+')"><i class="fa-solid fa-xmark Close" aria-hidden=true"></i>'+'</button>')
             .append('<hr/>');            
@@ -28,6 +28,34 @@ function retirerPanier(id) {
                 '<h6>Total: '+ (result.valeur).toFixed(2) + '</h6>');
         }
     });
+}
+
+function verifierProduitInventaire(id) {
+    let TOKEN_PANIER = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZENsaWVudCI6MSwicm9sZSI6ImNsaWVudCIsImlhdCI6MTYzNjc1MjI1MywiZXhwIjoxODM2NzUyMjUzfQ.qMcKC0NeuVseNSeGtyaxUvadutNAfzxlhL5LYPsRB8k";    
+
+    $.ajax({
+        url: "/produits/"+id,
+        method: "GET",
+        //data:{'quantite':-1},
+        beforeSend: function (xhr){
+            xhr.setRequestHeader('Authorization', "Basic "+ TOKEN_PANIER);
+        },
+        success: function( result ) {
+            console.log(result);  
+            
+            if (result.qte_inventaire <= 0) {
+                alert("Épuisé: "+ result.qte_inventaire);
+                //throw "Le produit est épuisé!";
+                return result.qte_inventaire;
+            }
+            else {
+                alert("En stock: " + result.qte_inventaire);
+                return result.qte_inventaire;
+            }
+
+            //location.reload();                 
+        }
+    })
 }
 
 // Fonction desuete
@@ -54,7 +82,6 @@ function enleverItem(id) {
         quantite = quantite - 1;
         document.getElementById("qte" + id).innerHTML = quantite;    
         
-        // Ne fonctionne pas pour le input qte...
         var prix = document.getElementById("prix" + id).innerHTML;    
         var element = document.getElementById("total" + id).innerHTML = (quantite * prix).toFixed(2);   
         var grandTotal = document.getElementById("grandTotal").innerHTML;
@@ -76,35 +103,54 @@ function enleverItem(id) {
     })};            
 }
 
-function ajouterItem(id) {
-    let TOKEN_PANIER = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZENsaWVudCI6MSwicm9sZSI6ImNsaWVudCIsImlhdCI6MTYzNjc1MjI1MywiZXhwIjoxODM2NzUyMjUzfQ.qMcKC0NeuVseNSeGtyaxUvadutNAfzxlhL5LYPsRB8k";
-    
-    var quantite = document.getElementById("qte" + id).innerHTML;
-
-    quantite = parseInt(quantite) + 1;    
-    document.getElementById("qte" + id).innerHTML = quantite;        
-        
-    // Ne fonctionne pas pour le input qte...
-    var prix = document.getElementById("prix" + id).innerHTML;    
-    var element = document.getElementById("total" + id).innerHTML = (parseInt(quantite) * prix).toFixed(2);   
-    var grandTotal = document.getElementById("grandTotal").innerHTML;
-    grandTotal = parseFloat(grandTotal) + parseFloat(prix);
-
-    document.getElementById("grandTotal").innerHTML = grandTotal.toFixed(2);
-        
-    $.ajax({
-        url: "/clients/1/panier/"+id,
-        method: "PUT",
-        data:{'quantite':1},
-        beforeSend: function (xhr){
-            xhr.setRequestHeader('Authorization', "Basic "+ TOKEN_PANIER);
-        },
-        success: function( result ) {
-            console.log(result);            
-            //location.reload();                 
-        }
-    });
-}
+function ajouterItem(id, idProduit) {
+    let TOKEN_PANIER = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZENsaWVudCI6MSwicm9sZSI6ImNsaWVudCIsImlhdCI6MTYzNjc1MjI1MywiZXhwIjoxODM2NzUyMjUzfQ.qMcKC0NeuVseNSeGtyaxUvadutNAfzxlhL5LYPsRB8k";                        
+        $.ajax({
+            url: "/produits/"+idProduit,
+            method: "GET",
+            //data:{'quantite':-1},
+            beforeSend: function (xhr){
+                xhr.setRequestHeader('Authorization', "Basic "+ TOKEN_PANIER);
+            },
+            success: function( result ) {
+                console.log(result);  
+                
+                if (result.qte_inventaire < 2) {
+                    alert("Épuisé: "+ result.qte_inventaire);
+                    //throw "Le produit est épuisé!";
+                    //return result.qte_inventaire;
+                }
+                else {
+                    alert("En stock: " + result.qte_inventaire);
+                    //return result.qte_inventaire;
+                    $.ajax({
+                        url: "/clients/1/panier/"+id,
+                        method: "PUT",
+                        data:{'quantite':1},
+                        beforeSend: function (xhr){
+                            xhr.setRequestHeader('Authorization', "Basic "+ TOKEN_PANIER);
+                        },
+                        success: function( result ) {
+                            console.log(result);     
+                            var quantite = document.getElementById("qte" + id).innerHTML;
+            
+                            quantite = parseInt(quantite) + 1;    
+                            document.getElementById("qte" + id).innerHTML = quantite;        
+                                
+                            var prix = document.getElementById("prix" + id).innerHTML;    
+                            var element = document.getElementById("total" + id).innerHTML = (parseInt(quantite) * prix).toFixed(2);   
+                            var grandTotal = document.getElementById("grandTotal").innerHTML;
+                            grandTotal = parseFloat(grandTotal) + parseFloat(prix);
+            
+                            document.getElementById("grandTotal").innerHTML = grandTotal.toFixed(2);       
+                            //location.reload();                 
+                        }
+                    });
+                //}
+            }
+                }
+        });
+    }                              
 
 function chargerpanier() {
     let TOKEN_PANIER = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZENsaWVudCI6MSwicm9sZSI6ImNsaWVudCIsImlhdCI6MTYzNjc1MjI1MywiZXhwIjoxODM2NzUyMjUzfQ.qMcKC0NeuVseNSeGtyaxUvadutNAfzxlhL5LYPsRB8k";
